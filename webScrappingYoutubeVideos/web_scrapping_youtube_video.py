49c01c10f63e1a64516a28a7dfed908fc0429ddf
dantae74 <dantae74@gmail.com>
2019-04-18 00:30:59 +0900

    Create web_scrapping_youtube_video.py

---
+++ webScrappingYoutubeVideos/web_scrapping_youtube_video.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup as bs
+import requests
+
+base = "https://www.youtube.com/result?search_query="
+qstring = "boddingtons+advert"
+
+r = requests.get(base+qstring)
+
+page = r.text
+soup = bs(page, 'html.parser')
+vids = soup.findAll('a',attrs={'class':'yt-uix-tile-link'})
