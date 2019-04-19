f2157b7917298ff35a5c43c88aff324132b88c81
dantae <49467366+dantae74@users.noreply.github.com>
2019-04-19 11:19:41 +0900

    Update web_scrapping_youtube_video.py

--- webScrappingYoutubeVideos/web_scrapping_youtube_video.py
+++ webScrappingYoutubeVideos/web_scrapping_youtube_video.py
@@ -9,3 +9,29 @@ r = requests.get(base+qstring)
 page = r.text
 soup = bs(page, 'html.parser')
 vids = soup.findAll('a',attrs={'class':'yt-uix-tile-link'})
+
+videoList = []
+for v in vids:
+    tmp = 'https://www.youtube.com' + v['href']
+    videoList.append(tmp)
+
+count=0
+for item in videolist:
+ 
+    # increment counter:
+    count+=1
+ 
+    # initiate the class:
+    yt = YouTube(item)
+ 
+    # have a look at the different formats available:
+    #formats = yt.get_videos()
+ 
+    # grab the video:
+    video = yt.get('mp4', '360p')
+ 
+    # set the output file name:
+    yt.set_filename('Video_'+str(count))
+ 
+    # download the video:
+    video.download('./')
